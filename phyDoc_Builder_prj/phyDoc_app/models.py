ac48e299e9750420a18ad6703d59ec7fac81d9ec
Anusha <anusgowda050@gmail.com>
2022-07-06 14:20:41 +0530

    add gitignore file

--- phyDoc_Builder_prj/phyDoc_app/models.py
+++ phyDoc_Builder_prj/phyDoc_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
+class Document_templates(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=80)
+    Document_template_path = models.models.models.FilePathField(_(""), path=None, match=None, recursive=recursive, max_length=100)
+
+class Document_details(models.Model):
+    id = models.IntegerField(primary_key=True)
+    templateid = models.ForeignKey(Document_templates, on_delete=models.CASCADE)
+    field_name = models.CharField(max_length=80)
+    field_type = models.CharField(max_length=80)
+    isRequired = models.BooleanField(default=False)
